40d1fd4b40bb52e97f6b4f60d52b686a0b58a0b3
jakesoule <jakerobertsoule@gmail.com>
2025-08-28 00:23:00 -0400

    1.6 unicafe step 1

--- Part 1 - Introduction to React/unicafe/src/App.jsx
+++ Part 1 - Introduction to React/unicafe/src/App.jsx
@@ -1,34 +1,30 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+
+const Result = ({ title, amount }) => <p>{title} {amount}</p>
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
-    <>
+    <div>
+      <h1>give feedback</h1>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Button onClick={() => setGood(good + 1)} text='good'/>
+        <Button onClick={() => setNeutral(neutral + 1)} text='neutral'/>
+        <Button onClick={() => setBad(bad + 1)} text='bad'/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <h2>statistics</h2>
+      <div>
+        <Result title='good' amount={good} />
+        <Result title='neutral' amount={neutral} />
+        <Result title='bad' amount={bad} />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
