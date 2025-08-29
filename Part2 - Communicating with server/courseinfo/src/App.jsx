26b42871afc4df5fa602dcdec4fe796ceb0dc5b1
jakesoule <jakerobertsoule@gmail.com>
2025-08-28 22:05:09 -0400

    2.4: course information step 9

--- Part2 - Communicating with server/courseinfo/src/App.jsx
+++ Part2 - Communicating with server/courseinfo/src/App.jsx
@@ -1,4 +1,5 @@
 const Course = ({ course }) => {
+  const total = course.parts.reduce((s, p) => s + p.exercises, 0)
 
   return (
     <div>
@@ -8,46 +9,58 @@ const Course = ({ course }) => {
           {part.name} {part.exercises}
         </p>
       )}
+      <p><b>total of {total} exercises</b></p>
     </div>
   )
 }
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4
-      }
-    ]
-  }
+  const courses = [
+    {
+      id: 1,
+      name: 'Half Stack application development',
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
-  const total = course.parts.reduce((s, p) => s + p.exercises, 0)
-
-  return (
-    <div>
-      <Course course={course} />
-      <p><b>total of {total} exercises</b></p>
-    </div>
-  )
+  return courses.map(course => <Course course={course} />)
 }
 
 export default App
